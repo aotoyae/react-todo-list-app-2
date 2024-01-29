7e63778e1c0cbd1c9110e474ce137641ed09ad6c
kimsohyeon <aotoyae@gmail.com>
2024-01-29 20:34:17 +0900

    add: wrap으로 전체 감쌈

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <>
+    <div className="wrap">
       <Header />
       <Main />
       {/* <Footer /> */}
-    </>
+    </div>
   );
 }
 
